0d12c07e0be06ac4c43e9c93ec3378a39645ab83
Clarissa Xu <clarissa_xu24@outlook.com>
2017-08-11 13:19:43 +0000

    Finished!

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,26 @@
 class BankAccount
   # code here
+  attr_accessor :bank_account, :balance, :status
+  attr_reader :name
+  def initialize(balance = 1000, status = "open", name)
+    @name = name
+    @balance = balance
+    @status = status
+    end
+    def deposit(money)
+        @balance += money
+    end
+    def display_balance
+       return "Your Balance is $#{@balance}." 
+    end
+    def valid?()
+        if @balance > 0 and @status == "open"
+            return true
+        else
+            return false
+        end
+    end
+    def close_account()
+        @status = "closed"
+    end
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,47 @@
+require_relative 'bank_account'
+
 class Transfer
   # code here
+  attr_accessor :receiver, :amount, :sender, :status
+  def initialize(sender, receiver, transferAmount)
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @amount = transferAmount
+  end
+
+    def both_valid?
+    # || is OR && is AND
+      if @sender.valid? && @receiver.valid?
+          return true
+        else
+            return false
+      end
+    end
+    
+    def execute_transaction()
+        # puts @sender.valid?
+        if @status != "complete" && both_valid? && @sender.balance >= @amount
+            @sender.balance = @sender.balance -  @amount
+            @receiver.balance += @amount
+            @status = "complete"
+        else
+            @status = "rejected"
+            return "Transaction rejected. Please check your account balance."
+        end
+    end
+    def reverse_transfer()
+      if @status == "complete"
+        @receiver.balance -= @amount
+        @sender.balance  += @amount
+        @status = "reversed"
+      end
+    end
 end
+
+# bank1 = BankAccount.new("open","Cristina")
+# bank2 = BankAccount.new("Clarissa")
+
+# firstTransfer = Transfer.new(bank1, bank2, 100)
+# puts firstTransfer.execute_transaction
+# puts firstTransfer.reverse_transfer
